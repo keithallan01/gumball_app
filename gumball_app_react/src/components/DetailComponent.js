a659c137f225483dc7a607621577eef6915bd122
Keith Allan <keithallan01@gmail.com>
2019-03-08 23:41:50 +0000

    listings displaying on page

--- gumball_app_react/src/components/DetailComponent.js
+++ gumball_app_react/src/components/DetailComponent.js
@@ -5,15 +5,35 @@ const DetailComponent = (props) => {
 
     const listingDetail = props.listingsArr.map((listing) => {
         return (
-            <div key={listing.id}>
-                <p>{listing.item}</p>
-            </div>
+            <tr key={listing.id}>
+                <td>{listing.item}</td>
+                <td>{listing.description}</td>
+                <td>{listing.category}</td>
+                <td>{listing.askingPrice}</td>
+                <td><button className="btn-warning">Edit</button></td>
+                <td><button className="btn-danger">Delete</button></td>
+            </tr>
         )
     })
 
     return (
-        <div >
-            {listingDetail}
+        <div>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Asking Price (£)</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {listingDetail}
+                </tbody>
+            </table>
         </div>
     );
 }
