cb3bdc5606b1c75f94b781eb159838aa25f2448d
Mohamed Eshaftri <eshaftri@gmail.com>
2019-03-09 06:47:53 +0000

    add method to save customer to db

--- gumball_app_react/src/components/DetailComponent.js
+++ gumball_app_react/src/components/DetailComponent.js
@@ -1,21 +1,11 @@
-import React from 'react';
+import React from "react";
 
+const DetailComponent = props => {
+  const listingDetail = props.listingsArr.map(listing => {
+    return <div key={listing.id}>{listing.item}</div>;
+  });
 
-const DetailComponent = (props) => {
-
-    const listingDetail = props.listingsArr.map((listing) => {
-        return (
-            <div key={listing.id}>
-                <p>{listing.item}</p>
-            </div>
-        )
-    })
-
-    return (
-        <div >
-            {listingDetail}
-        </div>
-    );
-}
+  return <div>{listingDetail}</div>;
+};
 
 export default DetailComponent;
--- gumball_app_react/src/containers/CustomerContainer.js
+++ gumball_app_react/src/containers/CustomerContainer.js
@@ -1,33 +1,61 @@
-import React from 'react';
-import { Url } from 'url';
-import CustomerInfo from '../components/CustomerInfo';
+import React from "react";
+import CustomerInfo from "../components/CustomerInfo";
 
 class CustomerContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            customers: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: [],
+    };
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <h1>Customers</h1>
+        <CustomerInfo
+          customersArr={this.state.customers}
+          onCustomerSubmit={this.handleNewCustomerSubmit}
+          onCustomerUpdate={this.handleUpdateCustomer}
+
+        />
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:8080/customers";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => this.setState({ customers: data._embedded.customers }));
+  }
+
+  handleNewCustomerSubmit = data => {
+    fetch("http://localhost:8080/customers", {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
         }
-    }
+      }).then(res => res.json())
+      .then(response => console.log('Success:', JSON.stringify(response)))
+      .catch(error => console.error('Error:', error));
+  };
 
-    componentDidMount() {
-        const url = "http://localhost:8080/customers";
-        fetch(url)
-            .then(res => res.json())
-            .then(data => this.setState({ customers: data._embedded.customers }));
-    }
-
-
-
-
-    render() {
-        return (
-            <div>
-                <h1>Customers</h1>
-                <CustomerInfo customersArr={this.state.customers} />
-            </div>
-        );
-    }
+  handleUpdateCustomer = data => {
+      console.log(data.id)
+    fetch("http://localhost:8080/customers/"+ data.id, {
+        method: 'PATCH', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      .then(response => console.log('Success:', JSON.stringify(response)))
+      .catch(error => console.error('Error:', error));
+  };
 }
 
 export default CustomerContainer;
