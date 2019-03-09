dd5725c313e8e1dc81d5bc7cb8501bfa04d33c7d
Jamie Reid <wjreid85.jr@gmail.com>
2019-03-09 23:27:32 +0000

    Displays input forms

--- gumball_app_react/src/containers/ListingContainer.js
+++ gumball_app_react/src/containers/ListingContainer.js
@@ -4,67 +4,72 @@ import SearchComponent from '../components/SearchComponent';
 import ListingInfo from '../components/ListingInfo';
 
 class ListingContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            listings: []
-         }
+  constructor(props) {
+    super(props);
+    this.state = {
+      listings: []
     }
+  }
 
-    componentDidMount() {
-        const url = "http://localhost:8080/listings";
-        fetch(url)
-          .then(res => res.json())
-          .then(data =>  this.setState({ listings: data._embedded.listings}));
+  componentDidMount() {
+    const url = "http://localhost:8080/listings";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => this.setState({ listings: data._embedded.listings }));
+  }
+
+  render() {
+    return (
+      <div className="listing-container">
+        <h1>Listings</h1>
+        <SearchComponent />
+        {/* <DetailComponent listingsArr={this.state.listings}/> */}
+        <ListingInfo
+          listingsArr={this.state.listings}
+          onListingSubmit={this.handleNewListingSubmit}
+          onListingUpdate={this.handleUpdateListing}
+          onListingDelete={this.handleDeleteListing}
+        />
+      </div>
+
+    );
+  }
+  handleNewListingSubmit = data => {
+    fetch("http://localhost:8080/listings", {
+      method: "POST", // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json"
       }
+    })
+      .then(res => res.json())
+      .then(response => console.log("Success:", JSON.stringify(response)))
+      .catch(error => console.error("Error:", error));
+  };
 
-    render() { 
-        return ( 
-            <div className="listing-container">
-                <h1>Listings</h1> 
-                <SearchComponent />
-                {/* <DetailComponent listingsArr={this.state.listings}/> */}
-                <ListingInfo listingsArr={this.state.listings}/>
-            </div>
-            
-        );
-    }
-    handleNewListingSubmit = data => {
-        fetch("http://localhost:8080/listings", {
-          method: "POST", // or 'PUT'
-          body: JSON.stringify(data), // data can be `string` or {object}!
-          headers: {
-            "Content-Type": "application/json"
-          }
-        })
-          .then(res => res.json())
-          .then(response => console.log("Success:", JSON.stringify(response)))
-          .catch(error => console.error("Error:", error));
-      };
-    
-      handleUpdateListing = data => {
-        fetch("http://localhost:8080/listings/" + data.id, {
-          method: "PATCH", // or 'PUT'
-          body: JSON.stringify(data), // data can be `string` or {object}!
-          headers: {
-            "Content-Type": "application/json"
-          }
-        })
-          .then(res => res.json())
-          .then(response => console.log("Success:", JSON.stringify(response)))
-          .catch(error => console.error("Error:", error));
-      };
-    
-      
-      handleDeleteListing = data => {
-        fetch("http://localhost:8080/listings/" + data, {
-          method: "DELETE", // or 'PUT'
-          body: JSON.stringify(data), // data can be `string` or {object}!
-          headers: {
-            "Content-Type": "application/json"
-          }
-        })
-      };
+  handleUpdateListing = data => {
+    fetch("http://localhost:8080/listings/" + data.id, {
+      method: "PATCH", // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(response => console.log("Success:", JSON.stringify(response)))
+      .catch(error => console.error("Error:", error));
+  };
+
+
+  handleDeleteListing = data => {
+    fetch("http://localhost:8080/listings/" + data, {
+      method: "DELETE", // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  };
 }
- 
+
 export default ListingContainer;
