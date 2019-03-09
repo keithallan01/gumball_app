35cfef773a4587beb745e2d06abf982e0a6b1344
Mark Mackay <Mark_Mackay@yahoo.com>
2019-03-09 11:27:13 +0000

    Almost got a town search working

--- GumballApp/src/main/java/com/example/GumballApp/repositories/ListingRepositoryCustom.java
+++ GumballApp/src/main/java/com/example/GumballApp/repositories/ListingRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.GumballApp.repositories;
 
+import com.example.GumballApp.models.Listing;
+
+import java.util.List;
+
 public interface ListingRepositoryCustom {
+    List<Listing> getAllListingsFromTown(String town);
 }
