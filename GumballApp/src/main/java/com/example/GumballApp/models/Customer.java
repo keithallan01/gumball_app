5ad8ee8db2c2cf466c032b0abc3e282a5dd92c5e
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-03-11 14:55:51 +0000

    On cascade delete listings when customer deleted

--- GumballApp/src/main/java/com/example/GumballApp/models/Customer.java
+++ GumballApp/src/main/java/com/example/GumballApp/models/Customer.java
@@ -38,7 +38,6 @@ public class Customer {
     // TODO: JSON IGNOREPROPERTIES
     //
     @JsonIgnoreProperties("customer")
-//    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
 
 
