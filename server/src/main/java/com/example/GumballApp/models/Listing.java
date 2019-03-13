e16ba57d532434c2a907b63fc9924196212fb890
Mohamed Eshaftri <eshaftri@gmail.com>
2019-03-13 19:17:43 +0000

    change made

--- server/src/main/java/com/example/GumballApp/models/Listing.java
+++ server/src/main/java/com/example/GumballApp/models/Listing.java
@@ -1,5 +1,6 @@
 package com.example.GumballApp.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
